23ca6c6978c2f11d87859b8468251255b1524ef0
Singam Ravi Babu <Singam Ravi Babu>
2025-08-11 20:47:51 +0530

    added some data to notes and one file.

---
+++ 001_creating_lists.py
@@ -0,0 +1,21 @@
+# A list of temperatures recorded over a week
+temps = [23.8, 25.1, 22.5, 24.0, 26.3, 27.5, 28.0]
+
+# A list of stationary items from a book store
+stationary_items = ["notebook", "pen", "eraser",
+                     "pencil", "marker"]
+
+# A list of 5 scores in the class
+scores = [88, 92, 79, 85, 90]
+
+
+# An empty list
+empty_list = []
+
+# A list using repeatition operator
+repeated_items = [0] * 5
+print("Repeated items:", repeated_items)
+
+# A list of mixed data types
+mixed_list = [42, "hello", 3.14, True, None]
+print("Mixed list:", mixed_list)
--- Notes_on_Lists.md
+++ Notes_on_Lists.md
@@ -5,4 +5,10 @@
 ```
 my_list = [item1, item2, item3, ...]
 ```
+##### Referring items in a list
+- To refer to the items in a list, we use an index, where 0 refers the first item, 1 refers the second item, and 2 refer the third item, and so on.
+- We can also use negative values for an index, where -1 refers to the last item, -2 refers to the second last item, and so on.
 
+```
+my_list[index]
+```
