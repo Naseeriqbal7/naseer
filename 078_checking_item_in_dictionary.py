8d1c4544beac548479ef8e8a46c923587e2f52a6
naseer <naseeriqbal7@gmail.com>
2025-08-30 01:32:42 +1000

    commit

---
+++ 078_checking_item_in_dictionary.py
@@ -0,0 +1,11 @@
+countries = {
+    "IN" : "India",
+    "US" : "United States",
+    "CA" : "Canada",
+    "MX" : "Mexico",
+    "DE" : "Germany"
+}
+ 
+if "IN" in countries:
+    print("India is a top country of the world!")
+    
